eddc1612217bb44cbd040399687f04314647ba11
Anden <andencastillo@gmail.com>
2026-01-20 15:31:29 +0800

    Refactor auth class and update member functions

--- auth.h
+++ auth.h
@@ -4,21 +4,16 @@
 #include "common.h"
 #include <string>
 
-
 namespace network {
-
 	class auth {
-	public:
+	public:	
 		static bool login(const std::string& username, const std::string& password);
 		static const std::string& get_session_token();
-		static void set_credentials(const std::string& username, const std::string& password);
+		static const std::string& get_last_error();
 
 	private:
 		static std::string s_session_token;
-		static std::string s_username;
-		static std::string s_password;
+		static std::string s_last_error;
 	};
-
 } 
-
-#endif 
+#endif
