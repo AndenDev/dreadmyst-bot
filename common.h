42a8522ddf41acca78f777e7bedacf8b3c68c33b
Anden <andencastillo@gmail.com>
2026-01-20 15:33:57 +0800

    Refactor string structures and update login function
    Removed game_string and game_wstring structures, updated game_login function parameters.

--- common.h
+++ common.h
@@ -14,25 +14,6 @@
 #pragma comment(lib, "ws2_32.lib")
 #pragma comment(lib, "crypt32.lib")
 
-
-struct game_string {
-	union {
-		char m_buffer[16];
-		char* m_ptr;
-	};
-	size_t m_length;
-	size_t m_capacity;
-};
-
-struct game_wstring {
-	union {
-		WCHAR m_buffer[8];
-		WCHAR* m_ptr;
-	};
-	size_t m_length;
-	size_t m_capacity;
-};
-
 struct packet_buffer {
 	void* m_vtable;
 	void* m_unknown1;
@@ -45,11 +26,12 @@ typedef void(__thiscall* pfn_game_send_packet)(void*, void*);
 typedef char(__stdcall* pfn_game_login)(
 	WCHAR* server_url,
 	DWORD endpoint,
-	void* credentials,
-	void* unused,
+	void* username,
+	void* password,
 	void* out_token,
 	void* out_error
-	);
+);
+
 typedef int* (__cdecl* pfn_get_network_manager)();
 
 #endif 
