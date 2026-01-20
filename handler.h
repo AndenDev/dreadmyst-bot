82e85cf6c2cb67196c5e20f43ae07d90992fc6a2
Anden <andencastillo@gmail.com>
2026-01-20 16:05:58 +0800

    Rename get_machine_fingerprint to generate_random_fingerprint

--- handler.h
+++ handler.h
@@ -12,13 +12,12 @@ namespace network {
 		static bool send_auth_packet(const std::string& token);
 		static bool send_character_select(DWORD character_id);
 		
-		static std::string get_machine_fingerprint();
+		static std::string generate_random_fingerprint();
 
 	private:
 		static packet_buffer* create_packet_buffer(void* data, size_t size);
 		static void send_packet(packet_buffer* packet);
 	};
-
 } 
 
 #endif
