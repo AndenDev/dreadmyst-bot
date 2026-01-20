23c5ced6efd635dd1e94d236a79be5cfa5adf6af
Anden <andencastillo@gmail.com>
2026-01-20 15:30:43 +0800

    Refactor auth class to simplify login process

--- auth.cpp
+++ auth.cpp
@@ -1,79 +1,59 @@
 #include "auth.h"
 #include "manager.h"
-#include "string_utils.h"
 #include <stdio.h>
 
 namespace network {
 
-
 	std::string auth::s_session_token;
-	std::string auth::s_username;
-	std::string auth::s_password;
-
-	void auth::set_credentials(const std::string& username, const std::string& password) {
-		s_username = username;
-		s_password = password;
-	}
-
-	bool auth::login(const std::string& username, const std::string& password) {
-		pfn_game_login login_func = game::manager::get_login_func();
-
-		if (!login_func) {
-
-			return false;
-		}
-
-
-		game_wstring server;
-		utils::init_game_wstring(&server, L"dreadmyst.com");
-
-		game_wstring endpoint;
-		utils::init_game_wstring(&endpoint, L"/auth/auth.php");
-
-		game_string username_str;
-		utils::init_game_string(&username_str, username.c_str());
-
-		game_string password_str;
-		utils::init_game_string(&password_str, password.c_str());
-
-		game_string token;
-		memset(&token, 0, sizeof(token));
-		token.m_capacity = 15;
-
-		game_string error;
-		memset(&error, 0, sizeof(error));
-		error.m_capacity = 15;
-
-		char result = login_func(
-			(WCHAR*)&server,
-			(DWORD)&endpoint,
-			&username_str,
-			&password_str,
-			&token,
-			&error
-		);
-
-		if (result) {
-			const char* token_str = (token.m_capacity > 15) ? token.m_ptr : token.m_buffer;
-			s_session_token = token_str;
-		}
-		else {
-			const char* error_str = (error.m_capacity > 15) ? error.m_ptr : error.m_buffer;
-		}
-
-		
-		utils::free_game_string(&username_str);
-		utils::free_game_string(&password_str);
-		utils::free_game_string(&token);
-		utils::free_game_string(&error);
-		utils::free_game_wstring(&server);
-		utils::free_game_wstring(&endpoint);
-
-		return result != 0;
-	}
+	std::string auth::s_last_error;
 
 	const std::string& auth::get_session_token() {
 		return s_session_token;
 	}
 
-} 
+	const std::string& auth::get_last_error() {
+		return s_last_error;
+	}
+
+	bool auth::login(const std::string& username, const std::string& password) {
+		printf("[AUTH] Attempting authentication for user: %s\n", username.c_str());
+
+		pfn_game_login login_func = game::manager::get_login_func();
+		if (!login_func) {
+			s_last_error = "Game login function not found";
+			printf("[AUTH] Failed: %s\n", s_last_error.c_str());
+			return false;
+		}
+
+	
+		const wchar_t* server = L"dreadmyst.com";
+		const wchar_t* endpoint = L"/auth/auth.php";
+
+		char token_buffer[256] = { 0 };
+		char error_buffer[256] = { 0 };
+
+		
+		char result = login_func(
+			(WCHAR*)server,
+			(DWORD)endpoint,
+			(void*)username.c_str(),
+			(void*)password.c_str(),
+			(void*)token_buffer,
+			(void*)error_buffer
+		);
+
+		if (result) {
+			s_session_token = token_buffer;
+			s_last_error.clear();
+			printf("[AUTH] Authentication successful! Token: %s\n", s_session_token.c_str());
+		}
+		else {
+			s_last_error = error_buffer;
+			s_session_token.clear();
+			printf("[AUTH] Authentication failed: %s\n", s_last_error.c_str());
+		}
+
+		return result != 0;
+	}
+
+}
